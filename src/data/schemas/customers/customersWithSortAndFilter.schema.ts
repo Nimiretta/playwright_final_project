55c7f4eda01459f8e952d51ad9ab8ae5453246c7
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-06-12 12:36:27 +0200

    Feat: Add Json Schema for customers

---
+++ src/data/schemas/customers/customersWithSortAndFilter.schema.ts
@@ -0,0 +1,31 @@
+import { customerSchema } from './customer.schema';
+import { COUNTRIES } from 'data/customers';
+
+export const productsWithSortAndFilterSchema = {
+  type: 'object',
+  properties: {
+    Customers: {
+      type: 'array',
+      items: customerSchema.properties.Customer,
+    },
+    IsSuccess: { type: 'boolean' },
+    ErrorMessage: { type: ['string', 'null'] },
+    total: { type: 'number' },
+    page: { type: 'number' },
+    limit: { type: 'number' },
+    search: { type: 'string' },
+    country: {
+      type: 'array',
+      items: { type: 'string', enum: Object.values(COUNTRIES) },
+    },
+    sorting: {
+      type: 'object',
+      properties: {
+        sortField: { type: 'string' },
+        sortOrder: { type: 'string' },
+      },
+      required: ['sortField', 'sortOrder'],
+    },
+  },
+  required: ['Customers', 'total', 'page', 'limit', 'search', 'country', 'sorting', 'IsSuccess', 'ErrorMessage'],
+};
