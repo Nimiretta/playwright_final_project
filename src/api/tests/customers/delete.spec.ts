6068d98c64c1065339ab4dd974d3001bdda2c238
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-06-14 21:00:24 +0200

    Add API tests: Customers delete

--- src/api/tests/customers/delete.spec.ts
+++ src/api/tests/customers/delete.spec.ts
@@ -19,6 +19,11 @@ test.describe('[API] [Customers] [Delete]', () => {
     customer = await customersApiSetvice.create(token);
   });
 
+  test.afterEach(async ({ customersApiSetvice }) => {
+    if (customer._id) return;
+    await customersApiSetvice.delete(customer._id, token);
+  });
+
   test(
     '[1_C_DL_API] Should DELETE the customer by correct ID',
     { tag: [TAGS.SMOKE, TAGS.API, TAGS.REGRESSION] },
@@ -30,6 +35,7 @@ test.describe('[API] [Customers] [Delete]', () => {
       validateResponse(responseGetID, STATUS_CODES.NOT_FOUND, false, ERRORS.CUSTOMER_NOT_FOUND(customer._id));
     },
   );
+
   test(
     '[3_C_DL_API] Should NOT delete customer with a non-existent ID',
     { tag: [TAGS.SMOKE, TAGS.API, TAGS.REGRESSION] },
@@ -42,11 +48,6 @@ test.describe('[API] [Customers] [Delete]', () => {
         false,
         ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
       );
-
-      expect(responseDelete.body).toEqual({
-        IsSuccess: false,
-        ErrorMessage: ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
-      });
     },
   );
 
@@ -54,19 +55,57 @@ test.describe('[API] [Customers] [Delete]', () => {
     '[4_C_DL_API] Should NOT delete customer with an invalid ID',
     { tag: [TAGS.API, TAGS.REGRESSION] },
     async ({ customersController }) => {
-      const nonexistentID = '123';
-      const responseDelete = await customersController.delete(nonexistentID, token);
+      const invalidID = '123';
+      const responseDelete = await customersController.delete(invalidID, token);
       validateResponse(
         responseDelete as unknown as IResponse<IResponseFields>,
         STATUS_CODES.NOT_FOUND,
         false,
-        ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
+        ERRORS.CUSTOMER_NOT_FOUND(invalidID),
       );
-
-      expect(responseDelete.body).toEqual({
-        IsSuccess: false,
-        ErrorMessage: ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
-      });
+    },
+  );
+  test(
+    '[5_C_DL_API] Should NOT delete  customer with correct ID  and  expired authorization token',
+    { tag: [TAGS.API, TAGS.REGRESSION] },
+    async ({ customersController }) => {
+      const expiredToken =
+        'eyJpZCI6IjY4MDRmMjcyZDAwNmJhM2Q0NzVmYjNlMCIsInJvbGVzIjpbIlVTRVIiXSwiaWF0IjoxNzQ5NzE0MjIwLCJleHAiOjE3NDk4MDA2MjB9.5MDtQekidZ8aQNWYJULTJQkpcs5a2nT_1yvVMYGaaMU';
+      const responseDelete = await customersController.delete(customer._id, expiredToken);
+      validateResponse(
+        responseDelete as unknown as IResponse<IResponseFields>,
+        STATUS_CODES.UNAUTHORIZED,
+        false,
+        ERRORS.EXPIRED_TOKEN,
+      );
+    },
+  );
+  test(
+    '[6_C_DL_API]  Should NOT delete  customer with correct ID  and  empty  authorization token',
+    { tag: [TAGS.API, TAGS.REGRESSION] },
+    async ({ customersController }) => {
+      const emptyToken = '';
+      const responseDelete = await customersController.delete(customer._id, emptyToken);
+      validateResponse(
+        responseDelete as unknown as IResponse<IResponseFields>,
+        STATUS_CODES.UNAUTHORIZED,
+        false,
+        ERRORS.EMPTY_TOKEN,
+      );
+    },
+  );
+  test(
+    '[7_C_DL_API]   Should NOT delete  customer with correct ID  and  incorrect / invalid  authorization token',
+    { tag: [TAGS.API, TAGS.REGRESSION] },
+    async ({ customersController }) => {
+      const incorrectToken = '12345';
+      const responseDelete = await customersController.delete(customer._id, incorrectToken);
+      validateResponse(
+        responseDelete as unknown as IResponse<IResponseFields>,
+        STATUS_CODES.UNAUTHORIZED,
+        false,
+        ERRORS.INVALID_TOKEN,
+      );
     },
   );
 });
--- src/data/errors.data.ts
+++ src/data/errors.data.ts
@@ -1,3 +1,6 @@
 export const ERRORS = {
   CUSTOMER_NOT_FOUND: (id: string) => `Customer with id '${id}' wasn't found`,
+  EMPTY_TOKEN: 'Not authorized',
+  INVALID_TOKEN: 'Invalid access token',
+  EXPIRED_TOKEN: 'Access token expired',
 } as const;
