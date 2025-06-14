18d3c376018e4dabd90c44e8f1ecd61ee2760959
Vitaliya Tsitova <titovavitaliy@gmail.com>
2025-06-14 19:00:53 +0200

    Add new api tests:  customers delete

--- src/api/tests/customers/delete.spec.ts
+++ src/api/tests/customers/delete.spec.ts
@@ -4,6 +4,8 @@ import { validateDeleteResponse, validateResponse } from 'utils/validations/resp
 import { TAGS } from 'data/tags.data';
 import { STATUS_CODES } from 'data/statusCodes.data';
 import { ERRORS } from 'data/errors.data';
+import { ObjectId } from 'bson';
+import { IResponse, IResponseFields } from 'types/api.types';
 
 test.describe('[API] [Customers] [Delete]', () => {
   let token = '';
@@ -18,7 +20,7 @@ test.describe('[API] [Customers] [Delete]', () => {
   });
 
   test(
-    'Should DELETE the customer by correct ID',
+    '[1_C_DL_API] Should DELETE the customer by correct ID',
     { tag: [TAGS.SMOKE, TAGS.API, TAGS.REGRESSION] },
     async ({ customersController }) => {
       const responseDelete = await customersController.delete(customer._id, token);
@@ -26,6 +28,45 @@ test.describe('[API] [Customers] [Delete]', () => {
       expect(responseDelete.body).toBe('');
       const responseGetID = await customersController.getById(customer._id, token);
       validateResponse(responseGetID, STATUS_CODES.NOT_FOUND, false, ERRORS.CUSTOMER_NOT_FOUND(customer._id));
+    },
+  );
+  test(
+    '[3_C_DL_API] Should NOT delete customer with a non-existent ID',
+    { tag: [TAGS.SMOKE, TAGS.API, TAGS.REGRESSION] },
+    async ({ customersController }) => {
+      const nonexistentID = new ObjectId().toHexString();
+      const responseDelete = await customersController.delete(nonexistentID, token);
+      validateResponse(
+        responseDelete as unknown as IResponse<IResponseFields>,
+        STATUS_CODES.NOT_FOUND,
+        false,
+        ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
+      );
+
+      expect(responseDelete.body).toEqual({
+        IsSuccess: false,
+        ErrorMessage: ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
+      });
+    },
+  );
+
+  test(
+    '[4_C_DL_API] Should NOT delete customer with an invalid ID',
+    { tag: [TAGS.API, TAGS.REGRESSION] },
+    async ({ customersController }) => {
+      const nonexistentID = '123';
+      const responseDelete = await customersController.delete(nonexistentID, token);
+      validateResponse(
+        responseDelete as unknown as IResponse<IResponseFields>,
+        STATUS_CODES.NOT_FOUND,
+        false,
+        ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
+      );
+
+      expect(responseDelete.body).toEqual({
+        IsSuccess: false,
+        ErrorMessage: ERRORS.CUSTOMER_NOT_FOUND(nonexistentID),
+      });
     },
   );
 });
