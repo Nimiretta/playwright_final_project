d6c69a6c0f3e2cb7868a900770ee8d92374dca93
Tatiana Korol <tatjana.deriugina@gmail.com>
2025-06-15 12:17:58 +0200

    feat: add getAll test for products

--- src/api/services/index.ts
+++ src/api/services/index.ts
@@ -1,4 +1,4 @@
 export * from './signIn.api-service';
-export * from './orders.api_service';
+export * from './orders.api-service';
 export * from './products.api-service';
 export * from './customers.api-service';
