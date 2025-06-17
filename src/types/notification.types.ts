ecb5390a51778f87c971d77a05c66c32e960583b
Tatiana Korol <tatjana.deriugina@gmail.com>
2025-06-17 19:06:04 +0200

    feat: add notifications controller and interfaces

---
+++ src/types/notification.types.ts
@@ -0,0 +1,22 @@
+import { ORDER_NOTIFICATIONS } from 'data/orders';
+import { IResponseFields } from 'types';
+
+export type NotificationType = keyof typeof ORDER_NOTIFICATIONS;
+
+export type NotificationMsg = (typeof ORDER_NOTIFICATIONS)[NotificationType];
+
+export interface INotificationFromResponse {
+  _id: string;
+  userId: string;
+  type: NotificationType;
+  orderId: string;
+  message: NotificationMsg;
+  read: boolean;
+  createdAt: string;
+  expiresAt: string;
+  updatedAt: string;
+}
+
+export interface INotificationsResponse extends IResponseFields {
+  Notifications: INotificationFromResponse[];
+}
