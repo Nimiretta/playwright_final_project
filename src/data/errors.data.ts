c77d23afcfe935f7ea320dc37644798155a8eade
ida-que <i.am.anna.nikulina@gmail.com>
2025-06-20 01:22:04 +0200

    tests for creating products

--- src/data/errors.data.ts
+++ src/data/errors.data.ts
@@ -5,5 +5,7 @@ export const API_ERRORS = {
   EXPIRED_TOKEN: 'Access token expired',
   PRODUCT_NOT_FOUND: (id: string) => `Product with id '${id}' wasn't found`,
   CUSTOMER_BAD_REQUEST: 'Incorrect request body',
+  PRODUCT_BAD_REQUEST: 'Incorrect request body',
   CUSTOMER_EMAIL_ALREADY_EXIST: (email: string) => `Customer with email '${email}' already exists`,
+  PRODUCT_NAME_ALREADY_EXISTS: (name: string) => `Product with name '${name}' already exists`,
 } as const;
