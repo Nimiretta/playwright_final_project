98315712d456d4f444c8eb541a585e15b3b43edc
Tatiana Korol <tatjana.deriugina@gmail.com>
2025-06-21 00:33:16 +0200

    feat: add tests delete customer/product assigned to order

--- src/data/errors.data.ts
+++ src/data/errors.data.ts
@@ -6,4 +6,6 @@ export const API_ERRORS = {
   PRODUCT_NOT_FOUND: (id: string) => `Product with id '${id}' wasn't found`,
   CUSTOMER_BAD_REQUEST: 'Incorrect request body',
   CUSTOMER_EMAIL_ALREADY_EXIST: (email: string) => `Customer with email '${email}' already exists`,
+  PRODUCT_IN_ORDER: 'Not allowed to delete product, assigned to the order',
+  CUSTOMER_IN_ORDER: 'Not allowed to delete customer, assigned to the order',
 } as const;
