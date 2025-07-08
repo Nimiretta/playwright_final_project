72262d1f5cccb25967ff5634353d3a9d83ac70f3
Aleksandr Zhuk <aleksandrzhuk@MacBook-Pro-Aleksandr.local>
2025-07-08 22:23:38 +0300

    add more tests for customer details in order

--- src/ui/tests/integration/orders/customerDetailsInOrder.spec.ts
+++ src/ui/tests/integration/orders/customerDetailsInOrder.spec.ts
@@ -8,13 +8,14 @@ import { IOrderFromResponse } from 'types';
 test.describe('[Component] [Orders] [Customer Details]', () => {
   let token = '';
   let order: IOrderFromResponse;
-  test.beforeEach(async ({ ordersApiService, signInApiService }) => {
+  test.beforeEach(async ({ signInApiService }) => {
     token = await signInApiService.getAuthToken();
-    order = await ordersApiService.createDraft(token);
   });
+
   test.afterEach(async ({ ordersApiService }) => {
     await ordersApiService.clear(token);
   });
+
   mockData.forEach((el) =>
     test(el.testName, { tag: el.tag }, async ({ orderDetailsPage, mock }) => {
       await mock.orderDetails(el.response);
@@ -24,10 +25,12 @@ test.describe('[Component] [Orders] [Customer Details]', () => {
       expect(convertCustomerToUIData(el.response.Order.customer)).toMatchObject(customer);
     }),
   );
+
   test(
-    'Should display edit button if order is in the Draft status',
+    'Should display edit button if order is in Draft status',
     { tag: ['@003_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION, TAGS.SMOKE] },
-    async ({ orderDetailsPage }) => {
+    async ({ orderDetailsPage, ordersApiService }) => {
+      order = await ordersApiService.createDraft(token);
       await orderDetailsPage.openPage('ORDER_DETAILS', order._id);
       await orderDetailsPage.waitForOpened();
       expect(orderDetailsPage.editCutomerButton).toBeVisible();
@@ -35,7 +38,7 @@ test.describe('[Component] [Orders] [Customer Details]', () => {
   );
 
   test(
-    'Should not display edit button if order is in the In Process status',
+    'Should not display edit button if order is in In Process status',
     { tag: ['@004_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION] },
     async ({ orderDetailsPage, ordersApiService }) => {
       order = await ordersApiService.createInProcess(token);
@@ -48,9 +51,11 @@ test.describe('[Component] [Orders] [Customer Details]', () => {
   test(
     'Should open edit customer modal after clicking on the edit button',
     { tag: ['@005_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION, TAGS.SMOKE] },
-    async ({ orderDetailsPage }) => {
+    async ({ orderDetailsPage, ordersApiService }) => {
+      order = await ordersApiService.createDraft(token);
       await orderDetailsPage.openPage('ORDER_DETAILS', order._id);
       await orderDetailsPage.waitForOpened();
+      await orderDetailsPage.editCutomerButton.click();
       await orderDetailsPage.clickEditCustomerButton();
       orderDetailsPage.editCustomerModal.waitForOpened();
     },
@@ -59,13 +64,47 @@ test.describe('[Component] [Orders] [Customer Details]', () => {
   test(
     'Should send correct request after clicking customer order',
     { tag: ['@006_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION] },
-    async ({ orderDetailsPage }) => {
+    async ({ orderDetailsPage, ordersApiService }) => {
+      order = await ordersApiService.createDraft(token);
       await orderDetailsPage.openPage('ORDER_DETAILS', order._id);
       await orderDetailsPage.waitForOpened();
       const request = await orderDetailsPage.interceptRequest(apiConfig.ENDPOINTS.CUSTOMERS_ALL, () =>
         orderDetailsPage.clickEditCustomerButton(),
       );
       expect(request.url()).toBe(apiConfig.BASE_URL + apiConfig.ENDPOINTS.CUSTOMERS_ALL);
+    },
+  );
+
+  test(
+    'Should not display edit button if order is in Canceled status',
+    { tag: ['@007_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION] },
+    async ({ orderDetailsPage, ordersApiService }) => {
+      order = await ordersApiService.createCanceled(token);
+      await orderDetailsPage.openPage('ORDER_DETAILS', order._id);
+      await orderDetailsPage.waitForOpened();
+      expect(orderDetailsPage.editCutomerButton).not.toBeVisible();
+    },
+  );
+
+  test(
+    'Should not display edit button if order is in Partially Received status',
+    { tag: ['@008_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION] },
+    async ({ orderDetailsPage, ordersApiService }) => {
+      order = await ordersApiService.createPartiallyReceived(token);
+      await orderDetailsPage.openPage('ORDER_DETAILS', order._id);
+      await orderDetailsPage.waitForOpened();
+      expect(orderDetailsPage.editCutomerButton).not.toBeVisible();
+    },
+  );
+
+  test(
+    'Should not display edit button if order is in Received status',
+    { tag: ['@009_O_CM_UI', TAGS.UI, TAGS.VISUAL_REGRESSION] },
+    async ({ orderDetailsPage, ordersApiService }) => {
+      order = await ordersApiService.createReceived(token);
+      await orderDetailsPage.openPage('ORDER_DETAILS', order._id);
+      await orderDetailsPage.waitForOpened();
+      expect(orderDetailsPage.editCutomerButton).not.toBeVisible();
     },
   );
 });
