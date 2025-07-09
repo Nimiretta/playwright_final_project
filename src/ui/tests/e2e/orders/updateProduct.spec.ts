16cb174545526808c4ed61b043cd729a9e19404e
Tatiana Korol <tatjana.deriugina@gmail.com>
2025-07-09 19:52:12 +0200

    Revert "move convertApiDataToUIData to utils"
    This reverts commit 07cc875db197a67d9f3f5c1aa8710f622f667f31.

--- src/ui/tests/e2e/orders/updateProduct.spec.ts
+++ src/ui/tests/e2e/orders/updateProduct.spec.ts
@@ -2,7 +2,7 @@ import { apiConfig } from 'config';
 import { NOTIFICATIONS, STATUS_CODES, TAGS } from 'data';
 import { test, expect } from 'fixtures';
 import { IOrderFromResponse, IProductFromResponse } from 'types';
-import { convertProductToUIData } from 'utils';
+import { convertProductToUIData } from 'data/orders';
 
 test.describe('[UI] [E2E] [Orders] [Update Product]', () => {
   let token = '';
